c0be52197713d3aa07f34b4fe37fdef02f84ea85
Maxime MAITRE <maxime.maitre@parrot.com>
2016-06-02 14:08:53 +0200

    [BUILD] Use Android.mk generated by alchemy.
    Change-Id: I51c36247a7ac7623b9e3d6f74a6303075916beef
    Reviewed-on: https://gerrit.parrot.biz/40042
    Reviewed-by: Djavan Bertrand <djavan.bertrand@parrot.com>

--- Android.mk
+++ Android.mk
@@ -7,5 +7,6 @@ LOCAL_CFLAGS := -g
 LOCAL_MODULE := libarmedia_android
 LOCAL_SRC_FILES := JNI/c/ARMEDIA_JNI_VideoAtoms.c
 LOCAL_LDLIBS := -llog -lz
-LOCAL_SHARED_LIBRARIES := libARMedia-prebuilt
+LOCAL_SHARED_LIBRARIES := libARMedia
+
 include $(BUILD_SHARED_LIBRARY)
